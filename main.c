359c1b9a219e87923cb45165fdaf94c43e815a85
Aaron Hsu <aaronhsu4606@gmail.com>
2021-11-16 18:27:07 -0500

    assignment

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+int main() {
+  printf("Parent process started\n");
+
+  int pid = fork();
+  if (!pid) {
+    printf("Child process %d started\n", getpid());
+  }
+  int pid2;
+
+  if (pid) {
+    pid2 = fork();
+    if (!pid2) {
+      printf("Child process %d started\n", getpid());
+    }
+  }
+
+  srand(time(NULL));
+  int time_sleep = rand() % 4 + 2;
+  if (pid && pid2) {
+    int status;
+    int pid_stat = wait(&status);
+    printf("Child process %d has finished in %d seconds\n", pid_stat, WEXITSTATUS(status));
+    printf("Parent process ended\n");
+    return 0;
+  }
+
+  sleep(time_sleep);
+  printf("Child process %d finished sleeping\n", getpid());
+  return time_sleep;
+
+}
